f398956042ab7395ccee84c733fa735be6c09fd6
steven <langsford.steven@gmail.com>
2019-02-17 15:19:32 -0500

    by condition randomization, 1st pass, not tested yet

--- data/stim_to_js.R
+++ data/stim_to_js.R
@@ -4,5 +4,16 @@ stimsource.df <- read.csv("stimcollection1.csv", stringsAsFactors=FALSE)
 
 stimsource.df$sentence <- sapply(stimsource.df$sentence,function(x){return(gsub("\\'","\\\\'",x))})#Ok for now but you might also want to check for " marks too.
 
- cat(paste0("var stim = shuffle(['",paste(stimsource.df$sentence,collapse="','"),"'])"),
-     file="stimarr.js")
+stimsource.df <- stimsource.df%>%select(sentence,sentenceType,grammatical)%>%mutate(cellID=as.factor(paste0(sentenceType,ifelse(grammatical,"_canon","_notcanon"))))
+
+##old one-big-vector version
+ ## cat(paste0("var stim = shuffle(['",paste(stimsource.df$sentence,collapse="','"),"'])"),
+ ##     file="stimarr.js")
+
+sink(file="bycell.js")
+cat("var allcells = {")
+for(acell in unique(stimsource.df$cellID)){
+    cat(paste0(acell,":","shuffle(['",paste((stimsource.df%>%filter(cellID==acell))$sentence,collapse="','"),"']),")) #Ugh note you have to delete the last comma.
+}
+cat("}")
+sink()
