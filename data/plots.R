ebac841a2cd1d4a8f5b51c8cbf23523c389bf633
steven <langsford.steven@gmail.com>
2019-02-18 17:38:10 -0500

    turk1

--- data/plots.R
+++ data/plots.R
@@ -1,6 +1,37 @@
-
+#rm(list=ls())#To remove...
 source("readData.R")
-##ggplot(responses.df,aes(x=response))+geom_histogram()+facet_wrap(~questiontext)+
-ggsave(ggplot(responses.df,aes(x=response,fill=canon_status))+geom_bar(position="dodge")+facet_wrap(~questiontext),file="responsehists.png")
+##Convenience flags: make sure all plot var names end with .plot, then save all or print all to screen at the end of the file.
+saveplots <- FALSE
+viewplots <- FALSE
 
-ggsave(ggplot(ratingtypes.df,aes(x=grammatical,y=acceptable,color=canonical))+geom_point(size=3),file="ratingtype_comparison.png")
+responsehists.plot <- ggplot(responses.df,aes(x=response,fill=canon_status))+geom_bar(position="dodge")+facet_wrap(~questiontext)
+
+ratingcomparison_byitem.plot <- ggplot(ratingtypes.df,aes(x=grammatical,y=acceptable,color=canonical))+geom_point(size=3)
+
+bycondition.df <- responses.df%>%
+    mutate(condition=paste(stim_type,canon_status,sep="_"))%>%
+    group_by(condition,questiontext)%>%
+    summarize(meanresponse=mean(response),canon_status=canon_status[1])%>%
+    ungroup()%>%
+    spread(questiontext,meanresponse)%>%
+    mutate(sentencetype=sapply(condition,function(x){strsplit(x,"_")[[1]][[1]]}))#afterthought...
+
+ names(bycondition.df)=c("condition","canon_status","acceptability_rating","grammaticality_rating","sentencetype")
+
+ratingcomparison_bycondition.plot <- ggplot(bycondition.df,aes(x=acceptability_rating,y=grammaticality_rating,color=canon_status))+
+    geom_line(aes(x=acceptability_rating,y=acceptability_rating))+
+    geom_point(size=3)+
+    geom_text(aes(label=sentencetype),hjust=0, vjust=0)
+
+
+
+ratingdifference.plot <- ggplot(ratingtypes.df,aes(x=text,y=(is_acceptable-is_grammatical),color=stim_type,shape=canon_status))+
+    geom_hline(aes(yintercept=0))+
+    geom_point(aes())+
+    facet_grid(.~stim_type,scales="free")
+
+##For everything that obeys the .plot naming convention:
+for(aplot in grep("\\.plot",ls(),value=TRUE)){
+    if(viewplots){ x11(); print(eval(parse(text=aplot)))}
+    if(saveplots){ggsave(eval(parse(text=aplot)),file=paste0(aplot,".png"))}
+}
--- data/readData.R
+++ data/readData.R
@@ -7,24 +7,32 @@ demographics.df <- read.csv("raw/demographicsdata.csv")
 responses.df <- read.csv("raw/sentenceresponsedata.csv")%>%
     filter(response!="continue")%>%
     mutate(response=as.numeric(as.character(response)),
-           questiontext=ifelse(questiontext=="Is this an <em>acceptable</em> English sentence?","Acceptable?","Grammatical?")
+           questiontext=ifelse(questiontext=="Is this an <em>acceptable</em> English sentence?","is_acceptable","is_grammatical")
            )
 
 ##exclusions
 badids <- data.frame()
+## Demographics exclusions
+badids <- demographics.df%>%filter(age<=0)%>%select(ppntID) #clearly not cooperating, probably deliberately :-)
+badids <- badids%>%rbind(demographics.df%>%filter(!str_detect(language,"en"))%>%select(ppntID)) #Reported non english speaker. Admits misspellings though.
+##Super-duper fast:
+timetaken.df <- responses.df%>%group_by(ppntID)%>%summarize(beginTime=min(responseTime), endTime= max(responseTime), totalTime=(endTime-beginTime)/1000/60)
+badids <- badids%>%rbind(timetaken.df%>%filter(totalTime<4)%>%select(ppntID)) #Always intended to cut superfast responses, but the actual cut-point here is informed by the histogram of response times, ggplot(timetaken.df,aes(x=totalTime))+geom_histogram(binwidth=1)+geom_vline(aes(xintercept=4)). Hope this is not evil, makes sense to me?
+
 ##incomplete:
 responsecount <- responses.df%>%group_by(ppntID)%>%summarize(count=n())
-badids <- responsecount%>%filter(count!=max(count))%>%select(ppntID) #Safe to assume max count is the expected number of trials... right? Better like this, better code in a magic number (currently 62), or best to do either but protect with asserts?
+badids <- badids%>%rbind(responsecount%>%filter(count!=max(count))%>%select(ppntID)) #Safe to assume max count is the expected number of trials... right? Better like this, better code in a magic number (currently 62), or best to do either but protect with asserts?
 ##attention checks
 attncheck_goodsentence <- responses.df%>%filter(text=="Sarah expected to get a good grade.")%>%select(ppntID,response)
 attncheck_badsentence <- responses.df%>%filter(text=="Him would have been fired.")%>%select(ppntID,response)
 
 badids <- badids%>%rbind(attncheck_goodsentence%>%filter(response<4)%>%select(ppntID)%>%rbind(attncheck_badsentence%>%filter(response>1)%>%select(ppntID))%>%distinct)
+
 #Apply exclusions:
 responses.df <- responses.df%>%filter(!(ppntID%in%badids$ppntID))
 
 ##remove attn checks from responses: these are not analysed further.
-responses.df <- responses.df%>%filter(text!="Sarah expected to get a good grade.",text!="Him would have been fired.")
+responses.df <- responses.df%>%filter(text!="Sarah expected to get a good grade.",text!="Him would have been fired.",text!="We were sure that the teaching assistant. liked to meet before lecture.") #First two are attn checks, last one a flawed stim. RS pointed this out, appears only in the pilot data (which is included for now.)
 
 ##add canonical status:
 apriori.df <- read.csv("stimcollection1.csv")
@@ -32,11 +40,12 @@ responses.df$canon_status <- unlist(sapply(responses.df$text,function(x){return(
 responses.df$stim_type <- unlist(sapply(responses.df$text,function(x){return(apriori.df[as.character(apriori.df$sentence)==as.character(x),"sentenceType"])}))
 
 byitem.df <- responses.df%>%
-    group_by(questiontext,text)%>%
-    summarize(mean_response=mean(response),count=n()) #just taking the mean like this possibly not ideal! You're in raw likert land, consider standardizing by ppnt (iff that doesn't muddy the waters too much re state trace / comparisons between acc and gram patterns? Also, some of these are probably error-detected/error-missed mixtures! Might be important.
+    group_by(questiontext,text,stim_type)%>% #type does not vary after grouping by text, but handy to have around?
+    summarize(mean_response=mean(response),count=n())%>%
+    ungroup()#just taking the mean like this possibly not ideal! You're in raw likert land, consider standardizing by ppnt (iff that doesn't muddy the waters too much re state trace / comparisons between acc and gram patterns? Also, some of these are probably error-detected/error-missed mixtures! Might be important.
 
 
 ratingtypes.df <- byitem.df%>%select(-count)%>%spread(questiontext,mean_response)%>%left_join(responses.df%>%select(text,canon_status),by="text")
-names(ratingtypes.df) <- c("text","acceptable","grammatical","canonical")#just getting rid of the ? marks after Acceptable and Grammatical
+
 
 write.csv(ratingtypes.df,file="threeratingtypes.csv",row.names=FALSE)
