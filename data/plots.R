d22d8e96b34ef23e0f0472bd99a6fa2b172cecfa
steven <langsford.steven@gmail.com>
2019-02-19 15:43:42 -0500

    fun with plots

--- data/plots.R
+++ data/plots.R
@@ -1,7 +1,7 @@
-#rm(list=ls())#To remove...
+rm(list=ls())#To remove...
 source("readData.R")
 ##Convenience flags: make sure all plot var names end with .plot, then save all or print all to screen at the end of the file.
-saveplots <- FALSE
+saveplots <- TRUE
 viewplots <- FALSE
 
 responsehists.plot <- ggplot(responses.df,aes(x=response,fill=canon_status))+geom_bar(position="dodge")+facet_wrap(~questiontext)
@@ -24,14 +24,47 @@ ratingcomparison_bycondition.plot <- ggplot(bycondition.df,aes(x=acceptability_r
     geom_text(aes(label=sentencetype),hjust=0, vjust=0)
 
 
+byitemtype.df <- responses.df%>%
+    group_by(item_type,questiontext,stim_type)%>%
+    summarize(meanresponse=mean(response),canon_status=canon_status[1])%>%
+    ungroup()%>%
+    spread(questiontext,meanresponse)
+
+ratingcomparison_byitem.plot <- ggplot(byitemtype.df,aes(x=is_acceptable,y=is_grammatical,color=canon_status))+
+    geom_line(aes(x=is_acceptable,y=is_acceptable),color="black")+
+    geom_point(size=3)+
+    geom_text(aes(label=paste0(stim_type,":",item_type)),hjust=1, vjust=1)
+
 
 ratingdifference.plot <- ggplot(ratingtypes.df,aes(x=text,y=(is_acceptable-is_grammatical),color=stim_type,shape=canon_status))+
     geom_hline(aes(yintercept=0))+
     geom_point(aes())+
     facet_grid(.~stim_type,scales="free")
 
+
+ratingbyitemhist.plot <- (ggplot(ratingtypes.df,aes(x=is_acceptable))+geom_histogram()+ggtitle("Acceptability rating"))+
+    (ggplot(ratingtypes.df,aes(x=is_grammatical))+geom_histogram()+ggtitle("Grammaticality rating"))
+
+
+for(astimtype in unique(ratingtypes.df$stim_type)){
+    #crazy eval thing is just to keep the ".plot names for plots" convention, which makes plot saving DRY. Not sure if this is good, seems worth trying? If it's bad, sh*t like this is where it goes bad...
+       eval(parse(text=paste0(astimtype,"_AGhistograms.plot<-",
+"(ggplot(ratingtypes.df,aes(x=is_acceptable))+geom_histogram()+ggtitle(paste0(astimtype,\": acceptability rating task\"))+
+    geom_vline(data=ratingtypes.df%>%filter(stim_type==astimtype),aes(xintercept=is_acceptable,color=item_type),alpha=.3)+
+    geom_vline(data=ratingtypes.df%>%filter(stim_type==astimtype)%>%group_by(item_type)%>%summarize(mean_acceptability=mean(is_acceptable))%>%ungroup(),
+               aes(xintercept=mean_acceptability,color=item_type),size=2)+guides(color=FALSE))+
+(ggplot(ratingtypes.df,aes(x=is_grammatical))+geom_histogram()+ggtitle(paste0(astimtype,\": grammaticality rating task\"))+
+    geom_vline(data=ratingtypes.df%>%filter(stim_type==astimtype),aes(xintercept=is_grammatical,color=item_type),alpha=.3)+
+    geom_vline(data=ratingtypes.df%>%filter(stim_type==astimtype)%>%group_by(item_type)%>%summarize(mean_acceptability=mean(is_grammatical))%>%ungroup(),
+               aes(xintercept=mean_acceptability,color=item_type),size=2))
+    ")))
+}
+
+
+
+##WRAPUP: Save or view loop
 ##For everything that obeys the .plot naming convention:
 for(aplot in grep("\\.plot",ls(),value=TRUE)){
     if(viewplots){ x11(); print(eval(parse(text=aplot)))}
-    if(saveplots){ggsave(eval(parse(text=aplot)),file=paste0(aplot,".png"))}
+    if(saveplots){ggsave(eval(parse(text=aplot)),file=paste0("plots/",aplot,".png"),width=15)}
 }
--- data/readData.R
+++ data/readData.R
@@ -38,14 +38,16 @@ responses.df <- responses.df%>%filter(text!="Sarah expected to get a good grade.
 apriori.df <- read.csv("stimcollection1.csv")
 responses.df$canon_status <- unlist(sapply(responses.df$text,function(x){return(apriori.df[as.character(apriori.df$sentence)==as.character(x),"grammatical"])}))
 responses.df$stim_type <- unlist(sapply(responses.df$text,function(x){return(apriori.df[as.character(apriori.df$sentence)==as.character(x),"sentenceType"])}))
+responses.df$item_type <- unlist(sapply(responses.df$text,function(x){return(apriori.df[as.character(apriori.df$sentence)==as.character(x),"itemType"])}))
+
 
 byitem.df <- responses.df%>%
-    group_by(questiontext,text,stim_type)%>% #type does not vary after grouping by text, but handy to have around?
+    group_by(questiontext,text,stim_type,item_type)%>% #stim/item type does not vary after grouping by text, but handy to have around?
     summarize(mean_response=mean(response),count=n())%>%
     ungroup()#just taking the mean like this possibly not ideal! You're in raw likert land, consider standardizing by ppnt (iff that doesn't muddy the waters too much re state trace / comparisons between acc and gram patterns? Also, some of these are probably error-detected/error-missed mixtures! Might be important.
 
 
-ratingtypes.df <- byitem.df%>%select(-count)%>%spread(questiontext,mean_response)%>%left_join(responses.df%>%select(text,canon_status),by="text")
+ratingtypes.df <- byitem.df%>%select(-count)%>%spread(questiontext,mean_response)%>%left_join(responses.df%>%select(text,canon_status),by="text")%>%distinct
 
 
 write.csv(ratingtypes.df,file="threeratingtypes.csv",row.names=FALSE)
